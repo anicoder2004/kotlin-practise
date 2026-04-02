ba37c8c940971e29912f9bb206f4e45fc323bbe1
Anicoder2004 <aniklancer2004@gmail.com>
2026-04-03 01:11:36 +0600

    LeapYear.kt is done

--- src/LeapYear.kt
+++ src/LeapYear.kt
@@ -1,5 +1,11 @@
 fun main(args: Array<String>) {
 
-
+    print("Enter the first number: ")
+    val year = readlnOrNull()?.toIntOrNull()
+    println("")
+    if (year == null) { println("Error! Enter a Valid Year!")
+        return}
+    else if (year % 4 == 0){ println("$year is a leap year")  }
+    else { println("$year is not a leap year") }
 
 }
