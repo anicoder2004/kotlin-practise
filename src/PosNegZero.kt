314302a6191d097032d6052802cef6c0cdc73889
Anicoder2004 <aniklancer2004@gmail.com>
2026-04-03 01:17:22 +0600

    PosNegZero.kt is done

--- src/PosNegZero.kt
+++ src/PosNegZero.kt
@@ -1,5 +1,14 @@
 fun main(args: Array<String>) {
 
-    
+    print("Enter a number: ")
+    val a = readlnOrNull()?.toIntOrNull()
+
+    println("")
+
+    if (a == null) { println("Error! Enter Only Real Numbers!")
+        return}
+    else if (a > 0){ println("$a is a positive number") }
+    else if (a < 0){ println("$a is a negative number") }
+    else { println("$a is Zero") }
 
 }
