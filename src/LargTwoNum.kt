e277c5709eae5f131933048d9f3215b2c6621f4c
Anicoder2004 <aniklancer2004@gmail.com>
2026-04-03 00:56:03 +0600

    LargTwoNum.kt is done

--- src/LargTwoNum.kt
+++ src/LargTwoNum.kt
@@ -1,5 +1,19 @@
 fun main(args: Array<String>) {
 
+    print("Enter the first number: ")
+    val a = readlnOrNull()?.toIntOrNull()
+    if (a == null) { println("Error! Enter Only Real Numbers!")
+        return}
 
+    print("Enter the second number: ")
+    val b = readlnOrNull()?.toIntOrNull()
+    if (b == null) { println("Error! Enter Only Real Numbers!")
+        return}
+
+    println("")
+
+    if (a > b) { println("The largest number is $a") }
+    else if (b > a) { println("The largest number is $b") }
+    else { println("Both numbers are equal: $a") }
 
 }
