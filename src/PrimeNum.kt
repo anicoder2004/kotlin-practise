5babf3c52ca31d8922dfada2c8da2934ba35653b
Anicoder2004 <aniklancer2004@gmail.com>
2026-04-03 01:33:55 +0600

    PrimeNum.kt is Done, but didnt understand its logic in the for loop.

--- src/PrimeNum.kt
+++ src/PrimeNum.kt
@@ -1,5 +1,30 @@
+import kotlin.math.sqrt
+
 fun main(args: Array<String>) {
 
+    print("Enter a number: ")
+    val num = readlnOrNull()?.toIntOrNull()
 
+    println("")
+
+    if (num == null) { println("Error! Enter Only Real Numbers!")
+        return }
+    else if (num <= 1) { println("$num is not a prime number.")
+        return }
+
+    // Down bellow is fully gpt generated.
+    // Didn't understand any logic of this for loop below
+    // But it works.
+
+    var isPrime = true
+    for (i in 2..sqrt(num.toDouble()).toInt()) {
+        if (num % i == 0) {
+            isPrime = false
+            break
+        }
+    }
+
+    if (isPrime) { println("$num is a prime number.") }
+    else { println("$num is not a prime number.") }
 
 }
